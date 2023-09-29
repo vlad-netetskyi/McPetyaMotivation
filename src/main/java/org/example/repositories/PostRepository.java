a8a41a86c6d2eb3bbe9091e0d538ce081df30ca9
Vladyslav Netetskyi <thermwaik@gmail.com>
2023-09-29 16:29:56 +0300

    added post repository

---
+++ src/main/java/org/example/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package org.example.repositories;
+
+import org.example.models.Post;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends CrudRepository<Post, Long> {
+}
