7d118119bfe90262b4bd3ea5c851b1587109e768
Vladyslav Netetskyi <thermwaik@gmail.com>
2023-09-29 16:36:02 +0300

    added post controller

---
+++ src/main/java/org/example/controllers/PostController.java
@@ -0,0 +1,48 @@
+package org.example.controllers;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Part;
+import org.apache.commons.io.IOUtils;
+import org.example.models.Post;
+import org.example.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Controller
+public class PostController {
+    @Autowired                                      // для чого це?
+    private PostRepository postRepository;
+
+    @GetMapping("/post")
+    public String post(HttpSession session, Model model) {
+
+        Iterable<Post> post = postRepository.findAll();
+        model.addAttribute("post", post);
+        return "post";
+    }
+
+    @GetMapping("/post/add")
+    public String getAddVehicles(Model model) {
+        return "post-add";
+    }
+
+    @PostMapping("/post/add")
+    public String AddVehicle(@RequestParam String description, @RequestParam Part img, Model mod) throws IOException {
+        InputStream fileContent = img.getInputStream();
+        byte[] fileAsByteArray = IOUtils.toByteArray(fileContent);
+        System.out.println("Added photo with size = " + fileAsByteArray.length);
+        Post post = new Post(description, fileAsByteArray);
+        postRepository.save(post);
+        return "redirect:/post";
+    }
+}
+
+
