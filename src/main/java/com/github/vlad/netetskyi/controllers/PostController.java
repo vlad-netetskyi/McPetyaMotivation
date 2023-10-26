7d334e269128607dca4467c32b916ccb5e70077d
Vladyslav Netetskyi <thermwaik@gmail.com>
2023-10-26 17:45:18 +0300

    added saving post with image to database

--- src/main/java/com/github/vlad/netetskyi/controllers/PostController.java
+++ src/main/java/com/github/vlad/netetskyi/controllers/PostController.java
@@ -3,6 +3,7 @@ package com.github.vlad.netetskyi.controllers;
 import com.github.vlad.netetskyi.controllers.models.Post;
 import com.github.vlad.netetskyi.repositories.PostRepository;
 import jakarta.servlet.http.HttpSession;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class PostController {
@@ -28,17 +30,18 @@ public class PostController {
     @GetMapping("/post/add")
     public String getAddPost(Model model) {
         model.addAttribute("post", new Post());
+        model.addAttribute("img", null);
         return "post-add";
     }
 
     @PostMapping("/post/add")
-    public String addPost(@ModelAttribute("post") Post post, Model model) {
+    public String addPost(@ModelAttribute("post") Post post, Model model) throws IOException {
         System.out.println("Post/add" + post.toString());
         try {
-          /*  InputStream fileContent = img.getInputStream();
+            InputStream fileContent = post.getImg().getInputStream();
             byte[] fileAsByteArray = IOUtils.toByteArray(fileContent);
-            System.out.println("Added photo with size = " + fileAsByteArray.length);*/
-            com.github.vlad.netetskyi.repositories.models.Post dbPost = new com.github.vlad.netetskyi.repositories.models.Post(post.getDescription());
+            System.out.println("Added photo with size = " + fileAsByteArray.length);
+            com.github.vlad.netetskyi.repositories.models.Post dbPost = new com.github.vlad.netetskyi.repositories.models.Post(post.getDescription(), fileAsByteArray);
             postRepository.save(dbPost);
 
             return "redirect:/post";
--- src/main/java/com/github/vlad/netetskyi/controllers/models/Post.java
+++ src/main/java/com/github/vlad/netetskyi/controllers/models/Post.java
@@ -1,13 +1,26 @@
 package com.github.vlad.netetskyi.controllers.models;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class Post {
     private String description;
+    // private byte[] img;
+    private MultipartFile img;
 
-    public Post(String description) {
+    public Post(String description, MultipartFile img) {
         this.description = description;
+        this.img = img;
     }
 
     public Post() {
+    }
+
+    public MultipartFile getImg() {
+        return img;
+    }
+
+    public void setImg(MultipartFile img) {
+        this.img = img;
     }
 
     public String getDescription() {
