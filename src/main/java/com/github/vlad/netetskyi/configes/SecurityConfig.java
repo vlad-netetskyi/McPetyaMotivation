52bc9b5e7b9b9acdeb118e42d52bdd17145a957f
Vladyslav Netetskyi <thermwaik@gmail.com>
2023-10-27 15:32:19 +0300

    added security filter for admin and user and form for login

--- src/main/java/com/github/vlad/netetskyi/configes/SecurityConfig.java
+++ src/main/java/com/github/vlad/netetskyi/configes/SecurityConfig.java
@@ -7,14 +7,42 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/login", "/post").permitAll()
+                        .requestMatchers("/post/add").hasRole("ADMIN") // Дозволяємо доступ до /post/add тільки адмінам
+                        .anyRequest().authenticated() // Всі інші URL вимагають аутентифікації
+                )
+                .formLogin(withDefaults()); // Використовуємо форму авторизації (логін)
 
+        return http.build();
+    }
 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
