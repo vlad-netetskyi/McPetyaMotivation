634f4ed54010b0c9ba34c523d2d939e5206e79da
Vladyslav Netetskyi <thermwaik@gmail.com>
2023-10-27 14:42:14 +0300

    create SecurityConfig

---
+++ src/main/java/com/github/vlad/netetskyi/configes/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.github.vlad.netetskyi.configes;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+
+
+}
